f7fcf73a74117dbff510a9bc87198d3a8fc1cd12
Jeff Chiu <jeffchiudev@gmail.com>
2025-07-17 20:27:39 -0700

    added sprite flip with directional change

--- Player.gd
+++ Player.gd
@@ -11,8 +11,12 @@ func _physics_process(delta: float) -> void:
 	velocity.x = 0
 	
 	if Input.is_key_pressed(KEY_LEFT):
+		if $Sprite	.flip_h == true: 
+			$Sprite	.flip_h = false
 		velocity.x -= move_speed
 	if Input.is_key_pressed(KEY_RIGHT):
+		$Sprite	.flip_h = true
+		
 		velocity.x += move_speed
 		
 	if Input.is_key_pressed(KEY_SPACE) and is_on_floor():
