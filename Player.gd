aaa2742c064b7c573f08c193a763c91e685f8e76
Jeff Chiu <jeffchiudev@gmail.com>
2025-07-17 20:14:11 -0700

    modified level tile map, created player node with collision and script with vars for movement, physics process for gravity and left/right movement

---
+++ Player.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+var move_speed : float = 100.0
+var jump_force : float = 200.0
+var gravity : float = 500.0
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		
+	velocity.x = 0
+	
+	if Input.is_key_pressed(KEY_LEFT):
+		velocity.x -= move_speed
+	if Input.is_key_pressed(KEY_RIGHT):
+		velocity.x += move_speed
+		
+	move_and_slide()
