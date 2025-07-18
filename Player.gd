ab538f917fbdd6b1a3645d3323a0eef50edfac1a
Jeff Chiu <jeffchiudev@gmail.com>
2025-07-17 20:33:16 -0700

    add game over when player falls below 100 y and restart scene

--- Player.gd
+++ Player.gd
@@ -11,15 +11,20 @@ func _physics_process(delta: float) -> void:
 	velocity.x = 0
 	
 	if Input.is_key_pressed(KEY_LEFT):
-		if $Sprite	.flip_h == true: 
-			$Sprite	.flip_h = false
+		if $Sprite.flip_h == true: 
+			$Sprite.flip_h = false
 		velocity.x -= move_speed
 	if Input.is_key_pressed(KEY_RIGHT):
-		$Sprite	.flip_h = true
-		
+		$Sprite.flip_h = true
 		velocity.x += move_speed
 		
 	if Input.is_key_pressed(KEY_SPACE) and is_on_floor():
 		velocity.y -= jump_force
 		
 	move_and_slide()
+	
+	if global_position.y > 100:
+		game_over()
+	
+func game_over ():
+	get_tree().reload_current_scene()
