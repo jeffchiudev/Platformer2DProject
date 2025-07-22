c030ab1dfa30038b4346f335291bd4bb2d6133f1
Jeff Chiu <jeffchiudev@gmail.com>
2025-07-21 23:32:21 -0700

    turned spike node into tscn. create coin node with sprite and collision

--- Player.gd
+++ Player.gd
@@ -4,6 +4,8 @@ var move_speed : float = 100.0
 var jump_force : float = 200.0
 var gravity : float = 500.0
 
+var score : int = 0
+
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -28,3 +30,6 @@ func _physics_process(delta: float) -> void:
 	
 func game_over ():
 	get_tree().reload_current_scene()
+	
+func add_score (amount):
+	score += amount
